dac258d169fb4ab662116c884224cd75d7bd4542
root <sachin@logbrains.com>
2025-12-05 12:54:41 +0530

    ssss

--- database/migrations/2025_12_04_073544_create_short_urls_table.php
+++ database/migrations/2025_12_04_073544_create_short_urls_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('short_urls', function (Blueprint $table) {
             $table->id();
             $table->string('url');
-            $table->integer('No_of_hits');
+            $table->integer('No_of_hits')->nullable();
             $table->integer('user_id');
             $table->integer('company_id');
             $table->timestamps();
--- database/migrations/2025_12_04_124354_add_client_id_to_users_table.php
+++ database/migrations/2025_12_04_124354_add_client_id_to_users_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            Schema::table('users', function (Blueprint $table) {
+                $table->unsignedBigInteger('client_id')->nullable()->after('role_id');
+            });
         });
     }
 
